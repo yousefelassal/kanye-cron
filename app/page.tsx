416101d40787440fa5ce1bf89e524edba26c0cae
Yousef Sameh <76617202+yousefelassal@users.noreply.github.com>
2024-01-12 21:42:47 +0000

    Add quote display functionality to page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,14 @@
-rfc
+import { readFile } from "fs/promises"
+
+export default async function page() {
+  const file = await readFile(process.cwd() + '/app/quote.json', 'utf8')
+  const quote = JSON.parse(file)
+  return (
+    <div className="min-h-screen w-screen flex flex-col items-center justify-center">
+        <div className="max-w-2xl w-full p-4">
+            <div className="font-semibold text-2xl">{quote.content}</div>
+            <div className="font-semibold text-lg">{quote.author}</div>
+        </div>
+    </div>
+  )
+}
